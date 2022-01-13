b4530707b54a889b5df6e742d4a48f40691a8498
louisLasserre <louis.lasserre@mmibordeaux.com>
2022-01-13 10:52:55 +0100

    v

--- Assets/changement de perso/PlayerChange.cs
+++ Assets/changement de perso/PlayerChange.cs
@@ -15,6 +15,12 @@ public class PlayerChange : MonoBehaviour
 
     public Transform posPlayer1;
     public Transform posPlayer2;
+
+    public BoxCollider2D BoxColl1;
+    public BoxCollider2D BoxColl2;
+
+    public Rigidbody2D RigidBody1;
+    public Rigidbody2D RigidBody2;
     
 
     public int chosenPlayer = 0;
@@ -35,6 +41,8 @@ public class PlayerChange : MonoBehaviour
         scriptBehave2.enabled = false;
 
         Anchor2.enabled = false;
+
+        bothPlayer();
 
         
 
@@ -91,13 +99,14 @@ public class PlayerChange : MonoBehaviour
                 else if (chosenPlayer == 1)
                 {
                     Debug.Log("vous controlez le perso  1");
-                    Anchor2.enabled = false;
+                    
 
                     perso1SR.color = new Color(1f, 1f, 1f, 1f);
                     perso2SR.color = new Color(0.47f, 0.47f, 0.47f);
 
-                    scriptBehave2.enabled = false;
-                    scriptBehave1.enabled = true;
+                    perso1SR.sortingLayerName = "PlayerInfront";
+
+                    
 
                     chrono = 0f;
                     confirm1 = true;
@@ -109,13 +118,14 @@ public class PlayerChange : MonoBehaviour
                 else if (chosenPlayer == 2)
                 {
                     Debug.Log("Vous controlez le perso 2");
-                    Anchor2.enabled = false;
+                    
 
                     perso1SR.color = new Color(0.47f, 0.47f, 0.47f);
                     perso2SR.color = new Color(1f, 1f, 1f, 1F);
 
-                    scriptBehave1.enabled = false;
-                    scriptBehave2.enabled = true;
+                    perso1SR.sortingLayerName = "PlayerInBack";
+
+
 
                     chrono = 0; 
                     confirm2 = true;
@@ -138,35 +148,45 @@ public class PlayerChange : MonoBehaviour
             {
 
 
-                Debug.Log("vous avez choisis de controler les deux persos ensembles");
-
-
-                Anchor2.enabled = true;
-
-                Debug.Log("tp de " + lastSoloPlayed);
-
-                HaveToGetTogether = false;
-
-
-                if (lastSoloPlayed == 1)
-                {
-                    posPlayer1.transform.position = posPlayer2.position;
-                }
-                else if (lastSoloPlayed == 2)
-                {
-                    posPlayer2.transform.position = posPlayer1.position;
-                }
+                bothPlayer();
             }
-            if(confirm1 == true){
+            if(confirm1 == true && chosenPlayer == 1){
                 lastSoloPlayed = 1;
+                
+                Anchor2.enabled = false;
                 confirm1 = false;
+
+                scriptBehave2.enabled = false;
+                scriptBehave1.enabled = true;
+
+                RigidBody2.constraints = RigidbodyConstraints2D.FreezeAll;
+                RigidBody1.constraints = RigidbodyConstraints2D.None;
+
+                BoxColl2.isTrigger = true;
+                BoxColl1.isTrigger = false;
+
+
+                
             }
-            if(confirm2 == true)
+            if(confirm2 == true && chosenPlayer == 2)
             {
                 lastSoloPlayed = 2;
+                
+                Anchor2.enabled = false;
+
+                scriptBehave1.enabled = false;
+                scriptBehave2.enabled = true;
+
+                RigidBody1.constraints = RigidbodyConstraints2D.FreezeAll;
+                RigidBody2.constraints = RigidbodyConstraints2D.None;
+
+                BoxColl1.isTrigger = true;
+                BoxColl2.isTrigger = false;
+               
                 confirm2 = false;
             }
         }
+
        
 
        
@@ -176,6 +196,51 @@ public class PlayerChange : MonoBehaviour
 
 
         
+
+    }//fin update
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        Debug.Log("sortit de la zone");
+    }
+
+    void bothPlayer()
+    {
+        Debug.Log("vous avez choisis de controler les deux persos ensembles");
+
+
+        Anchor2.enabled = true;
+
+        Debug.Log("tp de " + lastSoloPlayed);
+
+        BoxColl1.isTrigger = false;
+
+        RigidBody1.constraints = RigidbodyConstraints2D.None;
+        RigidBody2.constraints = RigidbodyConstraints2D.None;
+
+        perso1SR.sortingLayerName = "PlayerInfront";
+        perso2SR.color = new Color(0.47f, 0.47f, 0.47f);
+
+        HaveToGetTogether = false;
+
+
+        if (lastSoloPlayed == 1)
+        {
+            //posPlayer1.transform.position = posPlayer2.position;
+            BoxColl2.isTrigger = true;
+            posPlayer1.transform.position = new Vector2((posPlayer2.position.x + 70), posPlayer2.position.y);
+
+
+            //on exit de l'autre on réactive
+
+
+        }
+        else if (lastSoloPlayed == 2)
+        {
+            //posPlayer2.transform.position = posPlayer1.position;  **ancienne version de la ligne de tp
+            BoxColl2.isTrigger = true;
+            posPlayer2.transform.position = new Vector2((posPlayer1.position.x - 70), posPlayer1.position.y);
+
+        }
 
     }
 
